faa4b7d7ad0badadc8bdf02694a5c721efe9aaa0
Richard Marx <bellusci.richard@escola.pr.gov.br>
2025-11-09 16:48:34 -0300

    Fix typos and improve text clarity in alerts

--- Sociologia.js
+++ Sociologia.js
@@ -1,9 +1,9 @@
 
-    window.alert(`Olá, Bem vindo ao nosso site!`)
+    window.alert(`Olá, bem vindo ao nosso site!`)
    var nome= window.prompt(`Qual é o seu nome?`)
     window.alert(`Prazer em te conhecer ${nome}!`)
     window.alert(`Este site foi criado por Nycolas Ligoski, Pietro Czerski e Richard Marx`)
-    document.write(`Espero que tenha gostado do nosso site ${nome.toUpperCase()}! Para ver mais volte lá em cima e clique em "video educativo" onde você sera levado a uma pagina com um video explicando o preconceito e como afeta os outros, caso queira ver imagens de indviduos ou artes que ilustram essas pessoas`)
+    document.write(`Espero que tenha gostado do nosso site ${nome.toUpperCase()}! Para ver mais volte lá em cima e clique em "Vídeo educativo" onde você será levado a uma página com um video explicando o que é preconceito e como afeta os outros, caso queira ver imagens de indivíduos ou artes que ilustram essas pessoas que sofrem preconceito, clique em "Galeria da diversidade"`)
     dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
     monName = new Array ("janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro")  
     document.write ("<h1> Hoje é " + dayName[now.getDay() ] + ", " + now.getDate () + " de " + monName [now.getMonth() ]   +  " de "  +     now.getFullYear () + ". </h1>")
