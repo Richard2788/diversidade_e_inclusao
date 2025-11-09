9510045359342115214088aea39f870e921fb9e2
Richard Marx <bellusci.richard@escola.pr.gov.br>
2025-11-09 16:51:05 -0300

    Refine alert and document.write messages for clarity

--- Sociologia.js
+++ Sociologia.js
@@ -3,7 +3,7 @@
    var nome= window.prompt(`Qual é o seu nome?`)
     window.alert(`Prazer em te conhecer ${nome}!`)
     window.alert(`Este site foi criado por Nycolas Ligoski, Pietro Czerski e Richard Marx`)
-    document.write(`Espero que tenha gostado do nosso site ${nome.toUpperCase()}! Para ver mais volte lá em cima e clique em "Vídeo educativo" onde você será levado a uma página com um video explicando o que é preconceito e como afeta os outros, caso queira ver imagens de indivíduos ou artes que ilustram essas pessoas que sofrem preconceito, clique em "Galeria da diversidade"`)
+    document.write(`Espero que tenha gostado do nosso site ${nome.toUpperCase()}! Para ver mais, volte lá em cima e clique em "Vídeo educativo", você será levado a uma página com um video explicando o que é preconceito e como afeta os outros. Caso queira ver imagens de indivíduos ou artes que ilustrem essas pessoas que sofrem preconceito, clique em "Galeria da diversidade".`)
     dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
     monName = new Array ("janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro")  
     document.write ("<h1> Hoje é " + dayName[now.getDay() ] + ", " + now.getDate () + " de " + monName [now.getMonth() ]   +  " de "  +     now.getFullYear () + ". </h1>")
